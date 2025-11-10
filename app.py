677cc6d010ea35554482c2cd0596c93c3a601206
surprise213 <artem789123qwe@gmail.com>
2025-11-10 19:14:27 +0300

    feat(app): реализована загрузка данных врачей и расписания из БД, добавлены маршруты '/' и '/schedule'

--- app.py
+++ app.py
@@ -12,17 +12,44 @@ def get_users():
         con.row_factory = sqlite3.Row
         cursor = con.cursor()
         cursor.execute("""
-        SELECT d.name, s.name AS spec_name
+        SELECT d.name AS doctor_name,
+        d.photo AS doctor_photo,
+        s.name AS spec_name
         FROM doctors d
         JOIN specialize s ON d.id_spec = s.id
     """)
         doctors = cursor.fetchall()
     return doctors
 
+def get_schedule():
+    with sqlite3.connect("polic.db") as con:
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        cur.execute("""
+            SELECT 
+                d.nom_cab AS cabinet,
+                d.name AS doctor_name,
+                s.name AS specialty,
+                ws.monday, ws.tuesday, ws.wednesday, ws.thursday, ws.friday
+            FROM doctors d
+            JOIN specialize s ON d.id_spec = s.id
+            LEFT JOIN work_schedule ws ON ws.id_doctor = d.id
+            ORDER BY d.nom_cab
+        """)
+        data = cur.fetchall()
+    return data
+
 @app.route('/')
 def index():
-    doctors = get_users()
+    doctors = get_users()[:3]
     return render_template('structure.html', doctors=doctors)
+
+@app.route('/schedule')
+def schedule():
+    schedule_data = get_schedule()
+    return render_template('schedule.html', schedule_data=schedule_data)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
